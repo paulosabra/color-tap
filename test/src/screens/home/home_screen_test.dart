0506b9209590b6352319b8785632a37e7478b41a
Paulo Sabra <paulosabra.dev@gmail.com>
2025-04-06 19:04:27 -0300

    test(home): creates widget tests

---
+++ test/src/screens/home/home_screen_test.dart
@@ -0,0 +1,43 @@
+import 'package:color_tap/src/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('HomeScreen Widget Tests', () {
+    testWidgets('HomeScreen displays "Hello there" - Text Widget', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+      expect(find.text('Hello there'), findsOneWidget);
+    });
+
+    testWidgets('HomeScreen displays "Change Color" - Button Widget', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+      expect(find.text('Change Color'), findsOneWidget);
+    });
+
+    testWidgets('Tapping "Change Color" button changes colors', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+
+      final initialBackgroundColor =
+          tester.widget<Scaffold>(find.byType(Scaffold)).backgroundColor;
+      final initialTextColor =
+          tester.widget<Text>(find.text('Hello there')).style?.color;
+
+      await tester.tap(find.text('Change Color'));
+      await tester.pump();
+
+      final newBackgroundColor =
+          tester.widget<Scaffold>(find.byType(Scaffold)).backgroundColor;
+      final newTextColor =
+          tester.widget<Text>(find.text('Hello there')).style?.color;
+
+      expect(newBackgroundColor, isNot(initialBackgroundColor));
+      expect(newTextColor, isNot(initialTextColor));
+    });
+  });
+}
